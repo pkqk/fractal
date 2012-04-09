74679a1cddf74e13740fc264b5dd3034ed8ed55c
Adam Johnson <adam@pkqk.net>
2012-04-09 15:20:31 +0100

    start of a mandelbrot generator
    real	1m20.623s
    user	1m20.442s
    sys	0m0.127s

---
+++ mandelbrot.py
@@ -0,0 +1,46 @@
+from __future__ import print_function, unicode_literals, division
+from PIL import Image
+import sys
+import math
+
+
+class Mandelbrot(object):
+    def __init__(self, (left, top), (right, bottom)):
+        self.start = (left, top)
+        self.end = (right, bottom)
+
+    def map(self, width, height, x, y):
+        left, top = self.start
+        right, bottom = self.end
+        return complex(
+            left + (right - left) * ((x + 0.5) / width),
+            top - (top - bottom) * ((y + 0.5) / height)
+        )
+
+    def iterate(self, z):
+        iterations = 1000
+        c = z
+        for i in range(iterations):
+            z = z ** 2 + c
+            mag = z.real ** 2 + z.imag ** 2
+            if mag >= 4:
+                p = 255 - int(255 * (math.log(i + 1) /
+                    math.log(iterations)))
+                return (p, p, p)
+        return (0, 0, 0)
+
+    def draw_to(self, image):
+        width, height = image.size
+        for x in range(width):
+            for y in range(height):
+                zc = self.map(width, height, x, y)
+                colour = self.iterate(zc)
+                image.putpixel((x, y), colour)
+
+
+if __name__ == "__main__":
+    m = Mandelbrot((-2.5, 1.5), (1.5, -1.5))
+    im = Image.new('RGB', (1024, 768))
+    m.draw_to(im)
+    if len(sys.argv) > 1:
+        im.save(open(sys.argv[1], 'w'), 'PNG')
